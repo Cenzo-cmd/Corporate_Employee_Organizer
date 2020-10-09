e5a78fba2ba840002ab480cad95bd1235714f95d
Vincent <vrdphone@gmail.com>
2020-10-08 19:21:19 -0500

    added switch case menu with prompts

--- index.js
+++ index.js
@@ -9,3 +9,76 @@ const connection = mysql.createConnection({
     password: '',
     database: 'corporate_db'
 });
+
+connection.connect(err => {
+    if (err) throw err;
+
+    // console.log(`
+    // +--------------------------+
+    // | Welcome to Corporate_db! |
+    // +--------------------------+
+    // `);
+
+    start();
+});
+
+function start() {
+    const promptData = {
+        name: 'mainMenu',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View Employees',
+            'View Employees By Department',
+            'View Employees By Manager',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+            'Exit'
+        ]
+    }
+
+    inquirer.prompt(promptData).then(handleRespose);
+}
+
+function handleRespose(answers) {
+    switch (answers.mainMenu) {
+        case 'View Employees':
+            viewEmployees();
+            break;
+
+        case 'View Employees By Department':
+            console.log('view emp by dept')
+            break;
+
+        case 'View Employees By Manager':
+            console.log('view emp by manger');
+            break;
+
+        case 'Add Employee':
+            console.log('add emp');
+            break;
+
+        case 'Remove Employee':
+            console.log('remove emp');
+            break;
+
+        case 'Update Employee Role':
+            console.log('update emp role');
+            break;
+
+        case 'Update Employee Manager':
+            console.log('update emp manager');
+            break;
+
+        case 'Exit':
+            console.log('Goodbye!');
+            connection.end();
+            break;
+
+        default:
+            console.log('Connection ending');
+            connection.end();
+    }
+}
